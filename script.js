227541dc92de327f5fd9409cc4f097aeeef2fa6f
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-09-04 02:50:21 +0700

    commit

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ document.addEventListener('DOMContentLoaded' , () => {
             const li = document.createElement('li')
             li.innerHTML = inputBox.value
             listContainer.appendChild(li)
+            let span = document.createElement('span')
+            span.innerHTML = "\u00d7"
+            li.appendChild(span)
         }
     })
 
